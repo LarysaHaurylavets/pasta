f0a2e21d8934008ad5962df4d95d1aa9800e9a75
Larysa Haurylavets <Larysa_Haurylavets@epam.com>
2017-08-23 10:49:45 +0300

    Delete files

--- test/step_definitions/projectSettingsSteps.js
+++
@@ -1,9 +0,0 @@
-var {defineSupportCode} = require('cucumber');
-
-defineSupportCode(function({Given, When, Then}) {
-
-	When(/^I confirm the deletion of '([^']*)' project$/, function (projectName) {
-    return this.pageFactory.currentPage.confirmProjectDeletion(projectName);
-  });
-
-});
--- test/step_definitions/searchSteps.js
+++
@@ -1,9 +0,0 @@
-var {defineSupportCode} = require('cucumber');
-
-defineSupportCode(function({Given, When, Then}) {
-
-  Then(/^I check '([^']*)' include '([^']*)'$/, function(fieldName, value) {
-    return this.pageFactory.currentPage.checkValueOfTheElementInclude(fieldName, value);
-  });
-
-});
