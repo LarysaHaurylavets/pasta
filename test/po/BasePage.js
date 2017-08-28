d9289a4c7513939c3054fe5ac67847a40d644e1b
Eugen Stotik <eugen_stotik@epam.com>
2017-08-28 18:24:52 +0300

    Change address

--- test/po/BasePage.js
+++ test/po/BasePage.js
@@ -54,7 +54,7 @@ class BasePage {
       var pass = process.env.MY_PASS;
 
       var webdriver = require('selenium-webdriver');
-      var ad = new webdriver.Builder().usingServer('http://10.6.96.243:4723/wd/hub').withCapabilities({'browserName': 'AutoIt' }).build();
+      var ad = new webdriver.Builder().usingServer('http://127.0.0.1:4723/wd/hub').withCapabilities({'browserName': 'AutoIt' }).build();
       // var ad = new webdriver.Builder().usingServer('http://10.6.132.40:4723/wd/hub').withCapabilities({'browserName': 'AutoIt' }).build();
       // var ad = new webdriver.Builder().usingServer('http://localhost:4723/wd/hub').withCapabilities({'browserName': 'AutoIt' }).build();
 
