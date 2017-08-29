f6d40f1fc50feb01609b3e8d57189bf82ef43c0b
Eugen Stotik <eugen_stotik@epam.com>
2017-08-29 12:57:52 +0300

    Modify autoit driver address (third machine)

--- test/po/BasePage.js
+++ test/po/BasePage.js
@@ -53,7 +53,7 @@ class BasePage {
       var user = process.env.MY_USER;
       var pass = process.env.MY_PASS;
 
-      var webdriver = require('selenium-webdriver/remote');
+      var webdriver = require('selenium-webdriver');
       // var ad = new webdriver.Builder().usingServer('http://ecsc0010468b:4723/').withCapabilities({'browserName': 'AutoIt' }).build();
       // var ad = new webdriver.Builder().usingServer('http://127.0.0.1:4723/wd/hub').withCapabilities({'browserName': 'AutoIt' }).build();
       // var ad = new webdriver.Builder().usingServer('http://10.6.132.40:4723/wd/hub').withCapabilities({'browserName': 'AutoIt' }).build();
