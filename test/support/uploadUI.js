ad3517ae550273f09e3ce7160cee6ce1a591e875
Eugen Stotik <eugen_stotik@epam.com>
2017-08-29 17:46:02 +0300

    Remove remote ENV, add URL of eug

--- test/support/uploadUI.js
+++ test/support/uploadUI.js
@@ -14,7 +14,7 @@ var webdriver = require('selenium-webdriver');
 // process.env.SELENIUM_REMOTE_URL = 'http://10.6.97.105:4723/wd/hub';
 
 // eugen
-var ad = new webdriver.Builder('http://10.6.97.105:4723/wd/hub').usingServer().withCapabilities({'browserName': 'AutoIt' }).build();
+var ad = new webdriver.Builder().usingServer('http://10.6.96.243:4723/wd/hub').withCapabilities({'browserName': 'AutoIt' }).build();
 
 
 var filePaths = {
