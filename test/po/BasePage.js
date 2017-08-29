967cc6241908f7c634b176a053e7174c43157dbe
Larysa Haurylavets <Larysa_Haurylavets@epam.com>
2017-08-29 14:38:18 +0300

    Update files

--- test/po/BasePage.js
+++ test/po/BasePage.js
@@ -23,23 +23,46 @@ class BasePage {
         return browser.getCurrentUrl();
     }
 
+    // login() {
+    //   // return new Promise(function (resolve, reject)  {
+    //     // browser.sleep(8000);
+    //     console.log("run login script");
+    //     exec('./test/support/exe/login.exe', function (err, data) {
+    //     // exec('./login.exe', function (err, data) {
+    //         // console.log(err);
+    //         console.log(data.toString());
+    //     });
+    //     console.log("script finished");
+    //     return browser.sleep(5000);
+    //     // return resolve("login finished");
+
+    //   // });
+    // }
+
     login() {
-      // return new Promise(function (resolve, reject)  {
-        // browser.sleep(8000);
-        console.log("run login script");
-        exec('./test/support/exe/login.exe', function (err, data) {
-        // exec('./login.exe', function (err, data) {
-            // console.log(err);
-            console.log(data.toString());
-        });
-        console.log("script finished");
-        return browser.sleep(5000);
-        // return resolve("login finished");
+  browser.getTitle()
+    .then((txt) => console.log('title', txt));
 
-      // });
-    }
+  var user = process.env.MY_USER;
+  var pass = process.env.MY_PASS;
 
+  var webdriver = require('selenium-webdriver');
+//MY
+  var ad = new webdriver.Builder().usingServer('http://10.6.97.161:4723/wd/hub').withCapabilities({'browserName': 'AutoIt' }).build();
 
+  ad.switchTo().window("Authentication Required");
+  // ad.actions().sendKeys("{TAB}").perform();
+  browser.sleep(500);
+  ad.actions().sendKeys(user).perform();
+  browser.sleep(500);
+  ad.actions().sendKeys("{TAB}").perform();
+  browser.sleep(500);
+  ad.actions().sendKeys(pass).perform();
+  browser.sleep(500);
+  ad.actions().sendKeys("{ENTER}").perform();
+  browser.sleep(5000);
+
+}
 
 	//  login() {
 	// 	exec('./login.exe', [process.env.MY_USER, process.env.MY_PASS], function(err, data) {
