f80255730a0b47f76f7799b0afc4a4f6e1634152
Eugen Stotik <eugen_stotik@epam.com>
2017-08-29 17:38:12 +0300

    Webdriver remote ENV setup, remove URL

--- test/support/uploadUI.js
+++ test/support/uploadUI.js
@@ -10,11 +10,11 @@ var webdriver = require('selenium-webdriver');
 // var ad = new webdriver.Builder().usingServer('http://10.6.97.105:4723/wd/hub').withCapabilities({'browserName': 'AutoIt' }).build();
 
 
-//setup manually 
+//setup manually
 process.env.SELENIUM_REMOTE_URL = 'http://10.6.96.243:4723/wd/hub';
 
 // eugen
-var ad = new webdriver.Builder().usingServer('http://10.6.96.243:4723/wd/hub').withCapabilities({'browserName': 'AutoIt' }).build();
+var ad = new webdriver.Builder().usingServer().withCapabilities({'browserName': 'AutoIt' }).build();
 
 
 var filePaths = {
