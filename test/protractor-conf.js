137010afc0236a5f8e13a03ac1b66618f98e0803
Eugen Stotik <eugen_stotik@epam.com>
2017-08-31 11:50:05 +0300

    Add IP as default

--- test/protractor-conf.js
+++ test/protractor-conf.js
@@ -10,6 +10,7 @@ exports.config = {
 	framework: "custom",
 	frameworkPath: require.resolve("protractor-cucumber-framework"),
 	specs: ['features/*.feature'],
+
 	onPrepare: function() {
 		global.expect = chai.expect;
 		global.EC = protractor.ExpectedConditions;
@@ -22,7 +23,8 @@ exports.config = {
 			"eug" : 'http://10.6.96.243:4723/wd/hub'
 		};
 
-		global.AUTOIT_IP = autoit_ips[process.env.AUTOIT_HOST];
+//check if name is on the list, in other case use HOST as straight ip address
+		global.AUTOIT_IP = autoit_ips[process.env.AUTOIT_HOST] ? autoit_ips[process.env.AUTOIT_HOST] : process.env.AUTOIT_HOST + "/wd/hub";
 		console.log("\nAUTOIT EXECUTION IP: ", global.AUTOIT_IP);
 	},
 	cucumberOpts: {
